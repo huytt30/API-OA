5c4af856d84106708d00df5bbfc9c57785acca31
Huy <trantienhuy2001@gmail.com>
2024-12-09 02:09:13 +0700

    cm2

--- src/main/java/com/example/API_Zalo_OA/controller/zaloController.java
+++ src/main/java/com/example/API_Zalo_OA/controller/zaloController.java
@@ -3,11 +3,9 @@ package com.example.API_Zalo_OA.controller;
 
 import com.example.API_Zalo_OA.service.ZaloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/webhook")
@@ -18,6 +16,11 @@ public class zaloController {
     @Autowired
     public zaloController(ZaloService zaloService) {
         this.zaloService = zaloService;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getWebhookInfo() {
+        return ResponseEntity.ok("Webhook expects POST requests.");
     }
 
     @PostMapping
