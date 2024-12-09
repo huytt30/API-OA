9dcffd3f44b212260b2d961ae345bb471013c3ef
Huy <trantienhuy2001@gmail.com>
2024-12-09 15:30:22 +0700

    cm21

--- src/main/java/com/example/API_Zalo_OA/service/ZaloService.java
+++ src/main/java/com/example/API_Zalo_OA/service/ZaloService.java
@@ -41,29 +41,29 @@ public class ZaloService {
         return "CODE" + (int) (Math.random() * 10000);
     }
 
-    private void sendMessageToUser(String userId, String message) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("recipient", new JSONObject().put("user_id", userId));
-        jsonObject.put("message", new JSONObject().put("text", message));
-
-        String url = zaloApiUrl + "?access_token=" + accessToken;
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(jsonObject.toString(), headers);
-
-        try {
-            String response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
-            System.out.println("Response from Zalo API: " + response);
-
-            if (response == null || !response.contains("success")) {
-                System.err.println("Failed to send message: " + response);
-            }
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            System.err.println("Error sending message: " + e.getStatusCode() + " - " + e.getResponseBodyAsString());
-        } catch (Exception e) {
-            System.err.println("Error sending message: " + e.getMessage());
-        }
-    }
+//    private void sendMessageToUser(String userId, String message) {
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("recipient", new JSONObject().put("user_id", userId));
+//        jsonObject.put("message", new JSONObject().put("text", message));
+//
+//        String url = zaloApiUrl + "?access_token=" + accessToken;
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        HttpEntity<String> entity = new HttpEntity<>(jsonObject.toString(), headers);
+//
+//        try {
+//            String response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
+//            System.out.println("Response from Zalo API: " + response);
+//
+//            if (response == null || !response.contains("success")) {
+//                System.err.println("Failed to send message: " + response);
+//            }
+//        } catch (HttpClientErrorException | HttpServerErrorException e) {
+//            System.err.println("Error sending message: " + e.getStatusCode() + " - " + e.getResponseBodyAsString());
+//        } catch (Exception e) {
+//            System.err.println("Error sending message: " + e.getMessage());
+//        }
+//    }
 }
 
 
